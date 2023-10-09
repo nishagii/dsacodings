5752a8ccfec9c576b69acef11a5c2b848ec9549e
nishii <jeewanthadeherath04@gmail.com>
2023-10-09 10:01:37 +0530

    this is the first commit

---
+++ array.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#define MAX 5
+int arr[MAX];
+
+void insert(len){
+    int pos,val;
+    printf("where do you want to insert : ");
+    scanf("%d",&pos);
+
+    printf("what do you want to insert : ");
+    scanf("%d",&val);
+
+    for(int i=MAX;i<pos-1;i--){
+        arr[i+1]=arr[i];
+        arr[pos]=val;
+        
+    }
+    for(int i=0;i<len;i++){
+        printf(" %d ",arr[i]);
+    }
+}
+int main(){
+    int len;
+    printf("length of the array : ");
+    scanf("%d",&len);
+
+    for(int i=0;i<=len-1;i++){ 
+        printf("Enter element %d : ",i);
+        scanf("%d",&arr[i]);
+    }
+    insert(len);
+
+    
+    return 0;
+}
+
+
